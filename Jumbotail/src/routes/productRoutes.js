8b176b78eb4796fa7bd4928728a07f87134999fc
Punam Prajapati <punamprajapati1012@gmail.com>
2026-02-05 14:33:51 +0530

    fixed error

--- Jumbotail/src/routes/productRoutes.js
+++ Jumbotail/src/routes/productRoutes.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const {
+    createProduct,
+    getAllProducts,
+    getProductById,
+    updateProductMetadata,
+    updateProduct,
+    deleteProduct,
+    getProductStats,
     searchProductsController,
     searchByCategoryController
 } = require('../controllers/productController');
 
+// Search Routes
 router.get('/product', searchProductsController);
 router.get('/category/:category', searchByCategoryController);
+
+// CRUD Routes
+router.post('/add', createProduct);
+router.get('/list', getAllProducts);
+router.get('/stats', getProductStats);
+router.get('/:id', getProductById);
+router.put('/:id', updateProduct);
+router.patch('/metadata', updateProductMetadata);
+router.delete('/:id', deleteProduct);
 
 module.exports = router;
