9d3eb9ae36d4410b31a9eba41beac88c10c531e1
Punam Prajapati <punamprajapati1012@gmail.com>
2026-02-12 13:22:17 +0530

    deployment updated

--- Worko.ai/client/src/services/api.js
+++ Worko.ai/client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://company-assignment-1-mf6q.onrender.com/api"
 });
 
 API.interceptors.request.use(req => {
